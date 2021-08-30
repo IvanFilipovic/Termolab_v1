b1c0feb331a225015bc0d1b511c80a228f4c2eef
Ivan <ivan.filipovic233@gmail.com>
2021-08-30 23:36:56 +0200

    awd
    awd

--- form.php
+++ form.php
@@ -1,41 +1,14 @@
 <?php
+if(!empty($_POST["send"])) {
+	$email = $_POST["email"];
+	$content = $_POST["message"];
 
-$errors = [];
-$errorMessage = '';
-
-if (!empty($_POST)) {
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-
-
-    if (empty($email)) {
-        $errors[] = 'Email is empty';
-    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors[] = 'Email is invalid';
-    }
-
-    if (empty($message)) {
-        $errors[] = 'Message is empty';
-    }
-
-
-    if (empty($errors)) {
-        $toEmail = 'ivan.filipovic233@gmail.com';
-        $emailSubject = 'Zahtjev za povratnim odgovorom';
-        $headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=iso-8859-1'];
-
-        $bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message:", $message];
-        $body = join(PHP_EOL, $bodyParagraphs);
-
-        if (mail($toEmail, $emailSubject, $body, $headers)) {
-            header('Location: index.html');
-        } else {
-            $errorMessage = 'Oops, something went wrong. Please try again later';
-        }
-    } else {
-        $allErrors = join('<br/>', $errors);
-        $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
-    }
+	$toEmail = "ivan.filipovic233.com";
+	$mailHeaders = "From: " . $email . "<". $email .">\r\n";
+	if(mail($toEmail, $subject, $content, $mailHeaders)) {
+	    $message = "Your contact information is received successfully.";
+	    $type = "success";
+	}
 }
-
+require_once "contact-view.php";
 ?>
