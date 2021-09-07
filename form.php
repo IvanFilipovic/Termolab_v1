6d92ce23e90f044de4f0c62f016cbe0c123a92a5
Ivan <ivan.filipovic233@gmail.com>
2021-09-07 17:55:23 +0200

    za pokaazat
    6.1

--- form.php
+++ form.php
@@ -3,12 +3,15 @@ if(!empty($_POST["send"])) {
 	$email = $_POST["email"];
 	$content = $_POST["message"];
 
-	$toEmail = "ivan.filipovic233.com";
+	$toEmail = "ivan.filipovic233@gmail.com";
 	$mailHeaders = "From: " . $email . "<". $email .">\r\n";
 	if(mail($toEmail, $subject, $content, $mailHeaders)) {
 	    $message = "Your contact information is received successfully.";
 	    $type = "success";
 	}
+	else(
+		echo("Nije dobro")
+	)
 }
 require_once "contact-view.php";
 ?>
